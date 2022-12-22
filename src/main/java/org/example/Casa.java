7969c6c1b6ce3ada7f6af715af38b001049398cd
sxome <santixome@gmail.com>
2022-12-22 17:56:05 +0100

    Entidad Casa creada

---
+++ src/main/java/org/example/Casa.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Casa {
+    private String nombre;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
